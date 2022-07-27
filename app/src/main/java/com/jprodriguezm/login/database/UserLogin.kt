633ed796b07260b31f47994ff5243fd0c9541bb4
Juan Pablo Rodriguez Montoya <jprodriguezm90@gmail.com>
2022-07-26 19:08:21 -0500

    Initial commit Gradle, Fragments and data class created

---
+++ app/src/main/java/com/jprodriguezm/login/database/UserLogin.kt
@@ -0,0 +1,39 @@
+package com.jprodriguezm.login.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "users_login_table")
+data class UserLogin(
+    @PrimaryKey(autoGenerate = true)
+    var userId: Long = 0L,
+
+    @ColumnInfo(name = "email")
+    var email: String = "",
+
+    @ColumnInfo(name = "name")
+    var name: String = "",
+
+    @ColumnInfo(name = "password_hash")
+    var passwordHash: ByteArray? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UserLogin
+
+        if (passwordHash != null) {
+            if (other.passwordHash == null) return false
+            if (!passwordHash.contentEquals(other.passwordHash)) return false
+        } else if (other.passwordHash != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return passwordHash?.contentHashCode() ?: 0
+    }
+}
